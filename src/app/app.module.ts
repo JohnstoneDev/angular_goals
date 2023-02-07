4079fdc77466e238961a427140b356ecbdb0a397
JohnstoneDev <marucha.webdev@yahoo.com>
2023-02-07 16:05:05 +0300

    :feat Goal Component and GoalDetail Child Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoaldetailComponent } from './goaldetail/goaldetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent,
+    GoaldetailComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/goaldetail/goaldetail.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GoaldetailComponent } from './goaldetail.component';
+
+describe('GoaldetailComponent', () => {
+  let component: GoaldetailComponent;
+  let fixture: ComponentFixture<GoaldetailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GoaldetailComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GoaldetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
