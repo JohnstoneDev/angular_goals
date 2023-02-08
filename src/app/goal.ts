5400f0508aba50b15e0f207ede486712292c070f
JohnstoneDev <marucha.webdev@yahoo.com>
2023-02-08 09:53:20 +0300

    :feat Date Pipe

--- src/app/goal.ts
+++ src/app/goal.ts
@@ -1,7 +1,7 @@
 export class Goal {
     showDescription :  boolean = false; 
     
-    constructor(public id : number, public name : string, public description : string ){
+    constructor(public id : number, public name : string, public description : string, public commpleteDate : Date ){
         this.showDescription = false ; 
     }
 
