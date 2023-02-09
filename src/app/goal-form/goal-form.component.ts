ddbad8167bd5b37d29081cedee0b496b3e04e7d3
JohnstoneDev <marucha.webdev@yahoo.com>
2023-02-09 08:21:51 +0300

    :feat Create Goal Form Component

---
+++ src/app/goal-form/goal-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goal-form',
+  templateUrl: './goal-form.component.html',
+  styleUrls: ['./goal-form.component.css']
+})
+export class GoalFormComponent {
+
+}
